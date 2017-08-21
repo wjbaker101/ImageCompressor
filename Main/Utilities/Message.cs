c598f66ca5eb5d72d0a2e2c0f334293233dcf81e
wjbaker101 <willbaker10198@gmail.com>
2017-08-21 16:37:15 +0100

    Move Compressor and Utils class, Add coloured messages

---
+++ Main/Utilities/Message.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows.Media;
+
+namespace ImageCompressor.Main.Utilities
+{
+    public enum MessageType
+    {
+        ERROR,
+        WARNING,
+        SUCCESS,
+        DEFAULT
+    }
+
+    public class Message
+    {
+        public static Color GetColorFromType(MessageType messageType)
+        {
+            switch (messageType)
+            {
+                case MessageType.ERROR:
+                    return Color.FromArgb(255, 192, 23, 23);
+
+                case MessageType.WARNING:
+                    return Color.FromArgb(255, 255, 180, 0);
+
+                case MessageType.SUCCESS:
+                    return Color.FromArgb(255, 108, 192, 23);
+
+                case MessageType.DEFAULT:
+                default:
+                    return Color.FromArgb(255, 0, 0, 0);
+            }
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -7,12 +7,20 @@ using System.Windows.Controls;
 using Ookii.Dialogs.Wpf;
 using System.IO;
 using ImageCompressor.Main;
+using System.Windows.Media.Imaging;
+using ImageCompressor.Main.Utilities;
+using System.Windows.Media;
 
 namespace ImageCompressor
 {
     public partial class MainWindow : Window
     {
         private List<FileInfo> filesToCompress;
+
+        private int compressCount = 0;
+
+        private long sizeBefore = 0,
+                     sizeAfter = 0;
 
         public MainWindow()
         {
@@ -25,9 +33,7 @@ namespace ImageCompressor
         {
             VistaFolderBrowserDialog folderDialog = new VistaFolderBrowserDialog();
 
-            var result = folderDialog.ShowDialog();
-
-            if (result == true)
+            if (folderDialog.ShowDialog().Value)
             {
                 TextBox_InputDirectory.Text = folderDialog.SelectedPath;
 
@@ -63,7 +69,7 @@ namespace ImageCompressor
 
             ListBox_Items.ItemsSource = filesToCompress;
 
-            Label_Message.Content = "Found " + filesToCompress.Count + (filesToCompress.Count == 1 ? " image" : " images.");
+            ShowMessage("Found " + filesToCompress.Count + (filesToCompress.Count == 1 ? " image" : " images."), MessageType.SUCCESS);
         }
 
         private void CheckBox_Resize_CheckedChanged(object sender, RoutedEventArgs e)
@@ -75,13 +81,21 @@ namespace ImageCompressor
             };
 
             relatedControls.ForEach(control => control.IsEnabled = CheckBox_Resize.IsChecked.Value);
+
+            if (Button_Compress == null)
+                return;
+
+            if (CheckBox_Resize.IsChecked.Value)
+                Button_Compress.Content = "Compress and Resize";
+            else
+                Button_Compress.Content = "Compress";
         }
 
         private async void Button_Compress_Click(object sender, RoutedEventArgs e)
         {
             if (!Directory.Exists(TextBox_InputDirectory.Text))
             {
-                ShowMessage("Input directory does not exist.");
+                ShowMessage("Input directory does not exist.", MessageType.ERROR);
                 return;
             }
 
@@ -96,25 +110,25 @@ namespace ImageCompressor
             {
                 if (!Utils.IsInt(TextBox_Width.Text))
                 {
-                    ShowMessage("Width must be a number.");
+                    ShowMessage("Width must be a number.", MessageType.ERROR);
                     return;
                 }
 
                 if (Convert.ToInt32(TextBox_Width.Text) < 1 || Convert.ToInt32(TextBox_Width.Text) > 10000)
                 {
-                    ShowMessage("Width must be between 1 and 10,000, inclusive.");
+                    ShowMessage("Width must be between 1 and 10,000, inclusive.", MessageType.ERROR);
                     return;
                 }
 
                 if (!Utils.IsInt(TextBox_Height.Text))
                 {
-                    ShowMessage("Height must be a number.");
+                    ShowMessage("Height must be a number.", MessageType.ERROR);
                     return;
                 }
 
                 if (Convert.ToInt32(TextBox_Height.Text) < 1 || Convert.ToInt32(TextBox_Height.Text) > 10000)
                 {
-                    ShowMessage("Height must be between 1 and 10,000, inclusive.");
+                    ShowMessage("Height must be between 1 and 10,000, inclusive.", MessageType.ERROR);
                     return;
                 }
             }
@@ -123,6 +137,13 @@ namespace ImageCompressor
 
             if (result == MessageBoxResult.Yes)
             {
+                compressCount = 0;
+
+                sizeBefore = 0;
+                filesToCompress.ForEach(file => sizeBefore += file.Length);
+
+                Image_Preview.Source = null;
+
                 if (CheckBox_Resize.IsChecked.Value)
                 {
                     await ResizeImages();
@@ -153,10 +174,17 @@ namespace ImageCompressor
                 fileName = file.FullName;
                 destination = TextBox_OutputDirectory.Text + @"\" + file.Name;
 
-                resizeTasks.Add(Compressor.Instance.ResizeImage(fileName, destination, options));
+                resizeTasks.Add(ResizeImage(fileName, destination, options));
             });
 
             await Task.WhenAll(resizeTasks);
+        }
+
+        private async Task ResizeImage(string fileLocation, string destinationFile, dynamic options)
+        {
+            await Compressor.Instance.ResizeImageAsync(fileLocation, destinationFile, options);
+
+            ShowMessage("Resized " + ++compressCount + (filesToCompress.Count == 1 ? " image" : " images."), MessageType.DEFAULT);
         }
 
         private async Task CompressImages()
@@ -171,15 +199,38 @@ namespace ImageCompressor
                 fileName = file.FullName;
                 destination = TextBox_OutputDirectory.Text + @"\" + file.Name;
 
-                compressTasks.Add(Compressor.Instance.CompressImage(fileName, destination));
+                compressTasks.Add(CompressImage(fileName, destination));
             });
 
             await Task.WhenAll(compressTasks);
         }
 
-        private void ShowMessage(string message)
+        private async Task CompressImage(string fileLocation, string destinationFile)
+        {
+            await Compressor.Instance.CompressImageAsync(fileLocation, destinationFile);
+
+            ShowMessage("Compressed " + ++compressCount + (filesToCompress.Count == 1 ? " image" : " images."), MessageType.DEFAULT);
+        }
+
+        private void ListBox_Items_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Uri uriSource = new Uri(filesToCompress[ListBox_Items.SelectedIndex].FullName, UriKind.Absolute);
+
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            image.CacheOption = BitmapCacheOption.OnLoad;
+            image.CreateOptions = BitmapCreateOptions.IgnoreImageCache;
+            image.UriSource = uriSource;
+            image.EndInit();
+
+            Image_Preview.Source = image;
+        }
+
+        private void ShowMessage(string message, MessageType messageType)
         {
             Label_Message.Content = message;
+
+            Label_Message.Foreground = new SolidColorBrush(Message.GetColorFromType(messageType));
         }
     }
 }
